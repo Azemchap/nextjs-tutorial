b8399f3557e76a941199976c8b825d6ea46330c8
Azem <38106778+Azemchap@users.noreply.github.com>
2023-01-28 05:44:46 +0100

    updates

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,27 +1,133 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+// import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
 export const Header = () => {
   return (
-    <header>
-      <div className="fixed top-0 left-0 right-0 p-2 px-8 bg-teal-600 shadow-sm z-10">
-        <div className='max-w-screen-xl m-auto flex justify-between items-center '>
-          <Link href="/" passHref>
-            <Image className='block cursor-pointer' alt="logo" src={'/images/logo_black.png'} width={50} height={50} />
-          </Link>
+    <header className='fixed top-0 left-0 right-0 z-50 '>
+      <Disclosure as="nav" className="bg-white shadow ">
+        {({ open }) => (
+          <>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex justify-between h-16">
+                <div className="flex">
+                  <div className="flex-shrink-0 flex items-center mt-3">
+                    <Link href={`/`} passHref>
+                      <a >
+                        <Image
+                          className="block lg:hidden h-8 w-auto"
+                          src="/images/logo.png"
+                          alt="Workflow" width={100} height={25}
+                        />
+                      </a>
+                    </Link>
+                    {/* <Image
+                      className="hidden lg:block h-8 w-auto"
+                      src="/images/1.jpg"
+                      alt="Workflow" width={30} height={30}
+                    /> */}
+                  </div>
+                  <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                    {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                    <a
+                      href="#"
+                      className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    >
+                      Dashboard
+                    </a>
+                    <a
+                      href="#"
+                      className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    >
+                      Team
+                    </a>
+                    <a
+                      href="#"
+                      className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    >
+                      Projects
+                    </a>
+                    <a
+                      href="#"
+                      className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    >
+                      Calendar
+                    </a>
+                  </div>
+                </div>
+                <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                  <button
+                    type="button"
+                    className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  > <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                    </svg>
+                    <span className="sr-only">View notifications</span>
+                    {/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
+                  </button>
 
 
-          <nav className='flex gap-4 items-center'>
-            <Link href="/events" passHref>
-              <a> Events</a>
-            </Link>
+                </div>
 
-            <Link href="/about-us" passHref>
-              <a> About</a>
-            </Link>
-          </nav>
-        </div>
-      </div>
+
+                <div className="-mr-2 flex items-center sm:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="">
+                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                      </svg>
+                    ) : (
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="">
+                        <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+                      </svg>
+                    )}
+                  </Disclosure.Button>
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className="sm:hidden">
+              <div className="pt-2 pb-3 space-y-1">
+                {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  Dashboard
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  Team
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  Projects
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  Calendar
+                </Disclosure.Button>
+              </div>
+
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
     </header>
   )
 }
--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -1,17 +1,41 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { StarIcon } from '@heroicons/react/solid'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export const HomePage = ({ data }) => (
-  <div className="home_body flex flex-col gap-20 mt-10">
+  <div className=" grid gap-8 grid-cols-1 sm:mx-0 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
     {data?.map((ev) => (
       <Link key={ev.id} href={`/events/${ev.id}`} passHref>
-        <a className="card flex flex-col md:flex-row md:items-center gap-10" href={`/events/${ev.id}`}>
-          <div className="md:w-5/12 w-full">
+        <a className="flex flex-col h-auto" href={`/events/${ev.id}`}>
+          <div className="">
             <Image width={700} height={400} alt={ev.title} src={ev.image} />
           </div>
-          <div className=" md:w-5/12 w-full">
-            <h2 className='text-2xl mb-2'> {ev.title} </h2>
-            <p className='text-gray-700 text-lg'> {ev.description} </p>
+          <div className="">
+            <h2 className='text-2xl mb-2 font-bold '> {ev.title} </h2>
+            <p className='text-gray-600 text-lg h-20 overflow-hidden'> {ev.description} </p>
+          </div>
+          <div className='justify-self-end'>
+            <div className="mt-3 flex flex-col items-start">
+              <p className="sr-only">{ev.rating} out of 5 stars</p>
+              <div className="flex items-center">
+                {[0, 1, 2, 3, 4].map((rating) => (
+                  <StarIcon
+                    key={rating}
+                    className={classNames(
+                      ev.rating > rating ? 'text-yellow-400' : 'text-gray-200',
+                      'flex-shrink-0 h-5 w-5'
+                    )}
+                    aria-hidden="true"
+                  />
+                ))}
+              </div>
+              <p className="mt-1 text-sm text-gray-500">{ev.reviewCount} reviews</p>
+            </div>
+            <p className="mt-4 text-base font-medium text-gray-800">$ {ev.price} / XAF {ev.price * 550}</p>
           </div>
         </a>
       </Link>
